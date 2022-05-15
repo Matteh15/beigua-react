cee17064a829782be6d52cbc10ad8b361dbd49b1
Matteo La Licata <matteo.lalicata00@gmail.com>
2022-05-15 23:43:26 +0200

    Navbar desktop

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,13 @@
-import { AppBar, Toolbar, IconButton, Typography } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Stack,
+  Item,
+} from "@mui/material";
+
+import styled from "styled-components";
 
 import { mainGreen } from "../var";
 
@@ -6,12 +15,40 @@ import logo from "../img/logo.png";
 
 const Navbar = () => {
   return (
-    <AppBar style={{ background: mainGreen }}>
+    <AppBar style={{ background: mainGreen, padding: "1rem 10rem" }}>
       <Toolbar>
-        <IconButton size="small" edge="start" color="inherit" aria-label="logo">
-          <img src={logo} width="100px" color="white" />
-        </IconButton>
-        <Typography variant="h6" component="div"></Typography>
+        <Stack
+          direction="row"
+          spacing={2}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <IconButton
+            size="small"
+            edge="start"
+            color="inherit"
+            aria-label="logo"
+          >
+            <img src={logo} width="100px" color="white" />
+          </IconButton>
+          <Typography variant="h6" component="div" letterSpacing={5}>
+            SAVE PETS
+          </Typography>
+        </Stack>
+
+        <Stack
+          direction="row"
+          justifyContent="flex-end"
+          alignItems="center"
+          spacing={10}
+          flexGrow={1}
+        >
+          <p>HOME</p>
+          <p>CHI SIAMO</p>
+          <p>NOTIZIE</p>
+          <p>PETIZIONE</p>
+          <p>CONTATTI</p>
+        </Stack>
       </Toolbar>
     </AppBar>
   );
