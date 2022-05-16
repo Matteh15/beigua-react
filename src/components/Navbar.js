b26bc8f11b073e3d7082987aef3c9c3e044cd4b6
Matteo La Licata <matteo.lalicata00@gmail.com>
2022-05-16 17:40:02 +0200

    Tolto div vuoto e messo una sola appbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ import {
 import MenuIcon from "@mui/icons-material/Menu";
 import { EmailOutlined, Facebook, Instagram } from "@mui/icons-material";
 
-import { BrowserView, MobileView } from "react-device-detect";
+import { BrowserView, isMobile, MobileView } from "react-device-detect";
 import { mainGreen } from "../var";
 import { useState } from "react";
 
@@ -28,95 +28,21 @@ const Navbar = () => {
   const pages = ["HOME", "CHI SIAMO", "NOTIZIE", "PETIZIONE", "CONTATTI"];
 
   return (
-    <>
+    <AppBar
+      style={
+        isMobile
+          ? { background: mainGreen, padding: "0.5rem 0.5rem" }
+          : { background: mainGreen, padding: "1rem 5rem" }
+      }
+    >
       <BrowserView>
-        <AppBar style={{ background: mainGreen, padding: "1rem 5rem" }}>
-          <Toolbar>
-            <Stack
-              direction="row"
-              spacing={2}
-              justifyContent="center"
-              alignItems="center"
-            >
-              <IconButton
-                size="small"
-                edge="start"
-                color="inherit"
-                aria-label="logo"
-              >
-                <img src={logo} width="100px" color="white" />
-              </IconButton>
-              <Typography variant="h6" component="div" letterSpacing={5}>
-                SAVE PETS
-              </Typography>
-            </Stack>
-
-            <Stack
-              direction="row"
-              justifyContent="flex-end"
-              alignItems="center"
-              spacing={10}
-              flexGrow={1}
-            >
-              <MyBtn>HOME</MyBtn>
-              <MyBtn>CHI SIAMO</MyBtn>
-              <MyBtn>NOTIZIE</MyBtn>
-              <MyBtn>PETIZIONE</MyBtn>
-              <MyBtn>CONTATTI</MyBtn>
-            </Stack>
-          </Toolbar>
-        </AppBar>
-      </BrowserView>
-
-      <MobileView>
-        <AppBar style={{ background: mainGreen, padding: "1rem 0" }}>
-          <Drawer
-            open={openDrawer}
-            onClose={() => setOpenDrawer(!openDrawer)}
-            PaperProps={{
-              sx: {
-                backgroundColor: mainGreen,
-                padding: "2rem",
-              },
-            }}
-          >
-            {pages.map((value, index) => (
-              <MyBtn>{value}</MyBtn>
-            ))}
-
-            <Stack direction="row" alignItems="flex-end" flexGrow={1}>
-              <IconButton>
-                <Instagram style={{ color: "white" }} />
-              </IconButton>
-              <IconButton>
-                <Facebook style={{ color: "white" }} />
-              </IconButton>
-              <IconButton>
-                <svg
-                  style={{ width: "1em", height: "1em", fill: "white" }}
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 448 512"
-                >
-                  <path d="M448,209.91a210.06,210.06,0,0,1-122.77-39.25V349.38A162.55,162.55,0,1,1,185,188.31V278.2a74.62,74.62,0,1,0,52.23,71.18V0l88,0a121.18,121.18,0,0,0,1.86,22.17h0A122.18,122.18,0,0,0,381,102.39a121.43,121.43,0,0,0,67,20.14Z" />
-                </svg>
-              </IconButton>
-            </Stack>
-          </Drawer>
+        <Toolbar>
           <Stack
             direction="row"
-            justifyContent="space-around"
+            spacing={2}
+            justifyContent="center"
             alignItems="center"
           >
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-              onClick={setOpenDrawer}
-            >
-              <MenuIcon />
-            </IconButton>
             <IconButton
               size="small"
               edge="start"
@@ -125,18 +51,94 @@ const Navbar = () => {
             >
               <img src={logo} width="100px" color="white" />
             </IconButton>
-            <IconButton
-              size="small"
-              edge="start"
-              color="inherit"
-              aria-label="logo"
-            >
-              <EmailOutlined />
+            <Typography variant="h6" component="div" letterSpacing={5}>
+              SAVE PETS
+            </Typography>
+          </Stack>
+
+          <Stack
+            direction="row"
+            justifyContent="flex-end"
+            alignItems="center"
+            spacing={10}
+            flexGrow={1}
+          >
+            <MyBtn>HOME</MyBtn>
+            <MyBtn>CHI SIAMO</MyBtn>
+            <MyBtn>NOTIZIE</MyBtn>
+            <MyBtn>PETIZIONE</MyBtn>
+            <MyBtn>CONTATTI</MyBtn>
+          </Stack>
+        </Toolbar>
+      </BrowserView>
+
+      <MobileView>
+        <Drawer
+          open={openDrawer}
+          onClose={() => setOpenDrawer(!openDrawer)}
+          PaperProps={{
+            sx: {
+              backgroundColor: mainGreen,
+              padding: "2rem",
+            },
+          }}
+        >
+          {pages.map((value, index) => (
+            <MyBtn>{value}</MyBtn>
+          ))}
+
+          <Stack direction="row" alignItems="flex-end" flexGrow={1}>
+            <IconButton>
+              <Instagram style={{ color: "white" }} />
+            </IconButton>
+            <IconButton>
+              <Facebook style={{ color: "white" }} />
+            </IconButton>
+            <IconButton>
+              <svg
+                style={{ width: "1em", height: "1em", fill: "white" }}
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+              >
+                <path d="M448,209.91a210.06,210.06,0,0,1-122.77-39.25V349.38A162.55,162.55,0,1,1,185,188.31V278.2a74.62,74.62,0,1,0,52.23,71.18V0l88,0a121.18,121.18,0,0,0,1.86,22.17h0A122.18,122.18,0,0,0,381,102.39a121.43,121.43,0,0,0,67,20.14Z" />
+              </svg>
             </IconButton>
           </Stack>
-        </AppBar>
+        </Drawer>
+        <Stack
+          direction="row"
+          justifyContent="space-around"
+          alignItems="center"
+        >
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            onClick={setOpenDrawer}
+          >
+            <MenuIcon />
+          </IconButton>
+          <IconButton
+            size="small"
+            edge="start"
+            color="inherit"
+            aria-label="logo"
+          >
+            <img src={logo} width="100px" color="white" />
+          </IconButton>
+          <IconButton
+            size="small"
+            edge="start"
+            color="inherit"
+            aria-label="logo"
+          >
+            <EmailOutlined />
+          </IconButton>
+        </Stack>
       </MobileView>
-    </>
+    </AppBar>
   );
 };
 
