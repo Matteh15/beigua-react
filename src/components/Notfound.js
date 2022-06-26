d0d2ed7d4714d917dab6ef39100b3b3e51b8c2bb
CelesteBazzi <73994364+CelesteBazzi@users.noreply.github.com>
2022-06-26 17:39:16 +0200

    notfound

--- src/components/Notfound.js
+++ src/components/Notfound.js
@@ -30,19 +30,19 @@ export default function ComplexGrid() {
 
                 <Grid item xs container direction="column" spacing={0}>
                   <Grid item xs>
-                    <Typography component="div" color="white" fontFamily={"Poppins"} fontWeight={"800"} fontSize={"228px"} marginTop={"100px"} marginLeft={"100px"}>
+                    <Typography component="div" color="white" fontFamily={"Poppins"} fontWeight={"800"} fontSize={"228px"} marginTop={"90px"} marginLeft={"90px"}>
                       404
                     </Typography>
-                    <Typography  variant="h2" color="white" fontFamily={"Poppins"} fontWeight={"800"}  marginLeft={"100px"}>
+                    <Typography  variant="h2" color="white" fontFamily={"Poppins"} fontWeight={"800"}  marginLeft={"90px"}>
                       OPS!!! PAGINA NON TROVATA
                     </Typography>
-                    <Typography  color="white" fontFamily={"Poppins"} fontWeight={"800"} fontSize={"40px"}  marginLeft={"100px"}>
+                    <Typography  color="white" fontFamily={"Poppins"} fontWeight={"800"} fontSize={"40px"}  marginLeft={"90px"}>
                       Non siamo riusciti a trovare quello che stavi cercando potrebbe essere stato trivellato... 
                     </Typography>
-                    <Typography  color="white" fontFamily={"Montserrat"}  marginLeft={"100px"}>
+                    <Typography  color="white" fontFamily={"Poppins"}  marginLeft={"90px"} fontSize={"22px"} >
                       Non ti abbattere, Firma la petizione per salvare te e il tuo cucciolo.
                     </Typography>
-                    <Grid item marginTop={"30px"}  marginLeft={"100px"}>
+                    <Grid item marginTop={"30px"}  marginLeft={"90px"}>
                       <ButtonGreen variant="contained" href="#contained-buttons">Firma qui</ButtonGreen>
                       <ButtonWhite variant="contained" href="#contained-buttons" style={{color:'black'}} >Torna alla home</ButtonWhite>
                     </Grid>
