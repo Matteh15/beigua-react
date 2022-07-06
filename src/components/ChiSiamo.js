cfbc84afcfbe45fa6dce64855d6e0bea5b203ca7
Matte <matteo.lalicata00@gmail.com>
2022-07-06 22:57:01 +0200

    sistemato no redenre ChiSiamo

--- src/components/ChiSiamo.js
+++ src/components/ChiSiamo.js
@@ -10,75 +10,56 @@ import { BrowserView, MobileView } from "react-device-detect";
 import ButtonGreen from "./ButtonGreen";
 
 const ChiSiamo = () => {
-    return(
-        <>
-        <Container
-            maxWidth="100%"
-            style={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            }}
+  return (
+    <>
+      <Container
+        maxWidth="100%"
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <BoxStyled>
+          <Testo>
+            <h2
+              style={{
+                alignItems: "left",
+                fontFamily: "Poppins",
+                fontStyle: "normal",
+                fontWeight: "600",
+                fontSize: "62px",
+                lineHeight: "126%",
+                letterSpacing: "0.01em",
+                color: "#000000",
+              }}
             >
-            
-            <BoxStyled>
-                <Testo>
-                    <h2
-                        style={{
-                            alignItems: "left",
-                            fontFamily: 'Poppins',
-                            fontStyle: "normal",
-                            fontWeight: "600",
-                            fontSize: "62px",
-                            lineHeight: "126%",
-                            letterSpacing: "0.01em",
-                            color: "#000000",
-                        }}
-                    >
-                    Chi ChiSiamo
-                    </h2>
-                        <br></br>
+              Chi Siamo
+            </h2>
 
-                    <p
-                        style={{
-                            fontFamily: "Raleway",
-                            fontWeight: "400",
-                            fontSize: "18px",
-                            lineHeight: "30px",
-                            letterSpacing: "-0.015em",
-                            color: "#000000",
-                        }}
-                    >
-                    dsajsjadkjdsakjasdkjadsnbkdjsabsdakjbd
-                    asdbnkdsabnjkdasbkjdsa
-                    bkhdsakbasdkhbjdsa
-                    bjkdsajkbsad
-                    </p>
+            <p
+              style={{
+                fontFamily: "Raleway",
+                fontWeight: "400",
+                fontSize: "18px",
+                lineHeight: "30px",
+                letterSpacing: "-0.015em",
+                color: "#000000",
+              }}
+            >
+              dsajsjadkjdsakjasdkjadsnbkdjsabsdakjbd asdbnkdsabnjkdasbkjdsa
+              bkhdsakbasdkhbjdsa bjkdsajkbsad
+            </p>
 
-                    <ButtonGreen style={{ marginBottom: "3rem" }}>
-                    Leggi di pi&ugrave;
-                    </ButtonGreen>
-
-                </Testo>
-                <ContenitoreImmagine>
-                    dsadsadssadds
-                </ContenitoreImmagine>
-
-            </BoxStyled>
-
-
-        </Container>
-
-        <Container maxWidth="100%" style={{ backgroundColor: "#20202A" }}>
-            <div>
-            <H1ChiSiamo>Casi tra gli animali</H1ChiSiamo>
-            </div>
-            <div>
-
-            </div>
-        </Container>
-        </>
-    );
+            <ButtonGreen style={{ marginBottom: "3rem" }}>
+              Leggi di pi&ugrave;
+            </ButtonGreen>
+          </Testo>
+          <ContenitoreImmagine>dsadsadssadds</ContenitoreImmagine>
+        </BoxStyled>
+      </Container>
+    </>
+  );
 };
 
 const H1ChiSiamo = styled.h1`
@@ -88,35 +69,34 @@ const H1ChiSiamo = styled.h1`
 `;
 
 const Testo = styled.div`
-    margin:auto;
-    margin-top:25px;
-    margin-left:50px;
+  margin: auto;
+  margin-top: 25px;
+  margin-left: 50px;
 `;
 
 const ContenitoreImmagine = styled.div`
-    margin:auto;
-    width:80%;
-    height: 70%;
-
+  margin: auto;
+  width: 80%;
+  height: 70%;
 `;
 
-const BoxStyled = styled.Box`
-    display:grid;
-    grid-template-columns: 50% 50%;
-    justify-content: space-between;
-    align-items: center;
-    background-color: #FFFFFF;
-    border-radius: 20px;
-    box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.8);
-    width: 1269px;
-    height: 536px;
-    margin:auto;
-    margin-top:70px;
-    margin-bottom:70px;
+const BoxStyled = styled(Box)`
+  display: grid;
+  grid-template-columns: 50% 50%;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #ffffff;
+  border-radius: 20px;
+  box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.8);
+  width: 1269px;
+  height: 536px;
+  margin: auto;
+  margin-top: 70px;
+  margin-bottom: 70px;
 
-    @media screen and (max-width: 767px) {
-        display:grid;
-        grid-template-columns: 80%;
+  @media screen and (max-width: 767px) {
+    display: grid;
+    grid-template-columns: 80%;
   }
 `;
 
